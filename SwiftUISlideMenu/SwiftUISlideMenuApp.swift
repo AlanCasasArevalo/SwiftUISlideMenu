e0078c319989e220c6bbadec80363120d5174dc6
Alan <alan_casas_arevalo@gmail.com>
2021-05-10 10:40:06 +0200

    Initial Commit

---
+++ SwiftUISlideMenu/SwiftUISlideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISlideMenuApp.swift
+//  SwiftUISlideMenu
+//
+//  Created by Tecdata Engineering on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISlideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
